2f99636783ddce62dbdc06664b991ef789b8b7b7
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-12 23:24:36 +0900

    2023/01/12 백준 문제풀이 조건문 두수 비교

---
+++ BAEKJOON_01/C_Sharp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int[] INT = Array.ConvertAll(input, int.Parse);
+
+            if (INT[0] > INT[1])
+            {
+                Console.WriteLine(">");
+            }
+            else if(INT[0] < INT[1])
+            {
+                Console.WriteLine("<");
+            }
+            else
+            {
+                Console.WriteLine("==");
+            }
+        }
+    }
+}
