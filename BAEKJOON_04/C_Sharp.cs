d134d3f1b14594db3c2e25bb726eb06c7ab25377
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-16 17:35:21 +0900

    2023/01/16 백준문제풀이 (사분면 )

---
+++ BAEKJOON_04/C_Sharp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        int[] rel;
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();  
+            string y = Console.ReadLine();  
+            int xl = int.Parse(x);  
+            int yl = int.Parse(y);
+
+            if (xl > 0 && yl > 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if (xl < 0 && yl > 0)
+            {
+                Console.WriteLine("2");
+            }
+            else if (xl < 0 && yl< 0)
+            {
+                Console.WriteLine("3");
+            }
+            else
+            {
+                Console.WriteLine("4");
+            }
+
+
+        }
+    }
+}
