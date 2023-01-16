6fc291411db39e0d7a980a9dce36a655224386b2
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-16 16:31:17 +0900

    2023/01/16 백준문제풀이 (윤년)

---
+++ BAEKJOON_03/C_Sharp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input =Console.ReadLine();
+            int rel = int.Parse(input);
+
+            if ((rel % 4 == 0 && rel % 100 != 0) || rel % 400 == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
