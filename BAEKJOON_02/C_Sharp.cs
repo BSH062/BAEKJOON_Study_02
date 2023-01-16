2669535212dba31662c4c95323e0e2a42d7296b1
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-16 14:57:21 +0900

    2023/01/16 백준문제풀이 (조건문 범위)

---
+++ BAEKJOON_02/C_Sharp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int rel = int.Parse(input);
+
+            if (rel >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (89 >= rel || rel>=80)
+            {
+                Console.WriteLine("B");
+            }
+            else if (79 >= rel || rel >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else if (69 >= rel || rel >= 60)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+
+
+        }
+    }
+}
