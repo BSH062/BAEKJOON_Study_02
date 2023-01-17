91062ca8d8eb39dec99e945b5da9fd40610c4843
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-17 18:18:42 +0900

    2023/01/17 백준 문제풀이 (알람시계)

---
+++ BAEKJOON_05/C++.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	if (b >= 45)
+	{
+		b -= 45;
+		a--;
+	}
+	
+	else if (b < 45)
+	{
+		if (a == 0)
+		{
+			a += 23;
+			b = b + (60 - 45);
+		}
+		else
+		{
+			a--;
+			b = b + (60 - 45);
+		}
+	}
+	printf("%d %d", a, b);
+}
