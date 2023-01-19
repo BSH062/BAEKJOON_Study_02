a23cb30b60438421369e203e09d31fd1f4c220e5
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-19 17:45:16 +0900

    2023/01/19 백준 문제풀이 (오븐시계)

---
+++ BAEKJOON_06/C++.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int a, b, c,d;
+	scanf("%d %d", &a, &b);
+	scanf("%d", &c);
+
+	if (b+c >=60)
+	{
+		d = b + c;
+		a += d / 60;
+		b = d % 60;
+		if (a>=24)
+		{
+			a -= 24;
+		}
+
+	}
+	else if (b+c<60)
+	{
+		d = b + c;
+		b = d;
+	}
+	printf("%d %d", a, b);
+
+}
---
+++ BAEKJOON_06/C_Sharp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input =Console.ReadLine().Split(' ');
+            string wTime = Console.ReadLine();
+            int[] rel = Array.ConvertAll(input, int.Parse);
+            int wTimerel = int.Parse(wTime);
+
+            int total = wTimerel + rel[1];
+            if (total >= 60)
+            {
+                rel[0] += total / 60;
+                rel[1] = total % 60;
+                if (rel[0] >= 24)
+                {
+                    rel[0] -= 24;
+                }
+
+            }
+            else if (total < 60)
+            {
+                rel[1] = total;
+            }
+            Console.WriteLine(rel[0]+" " + rel[1]);
+
+
+        }
+    }
+}
