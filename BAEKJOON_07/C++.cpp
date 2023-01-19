2c7b65dcb2319c69e315276c92a76e5e38ffb859
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-19 18:16:27 +0900

    2023/01/19 백준문제풀이 (주사위 세개)

---
+++ BAEKJOON_07/C++.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int a[4];
+	int total;
+	scanf("%d %d %d", &a[0], &a[1], &a[2]);
+	if (a[0] == a[1] && a[1] == a[2])
+	{
+		total = 10000 + (a[0] * 1000);
+	}
+	else if (a[0] == a[1] || a[1] == a[2] || a[0] == a[2])
+	{
+		if (a[0] == a[1] || a[0] == a[2])
+		{
+			a[3] = a[0];
+		}
+		else
+		{
+			a[3] = a[1];
+		}
+		total = 1000 + (a[3] * 100);
+	}
+	else
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			if (a[i]>a[3])
+			{
+				a[3] = a[i];
+			}
+		}
+		total = a[3] * 100;
+	}
+	printf("%d", total);
+}
---
+++ BAEKJOON_07/C_Sharp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int[] rel = Array.ConvertAll(input, int.Parse);
+            int con=0;
+            int total;
+
+            if (rel[0] == rel[1] && rel[1] == rel[2])
+            {
+                total = 10000 + (rel[0] * 1000);
+            }
+            else if (rel[0] == rel[1] || rel[1] == rel[2] || rel[0] == rel[2])
+            {
+                if (rel[0] == rel[1] || rel[0] == rel[2])
+                {
+                    con = rel[0];
+                }
+                else
+                {
+                    con = rel[1];
+                }
+                total = 1000 + (con * 100);
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (rel[i] > con)
+                    {
+                        con = rel[i];
+                    }
+                }
+                total = con * 100;
+            }
+            Console.WriteLine(total); 
+        }
+    }
+}
